3e81714a5925888c513a5890d51d81e9dc4759d1
Mark Reilly <mark@alienresident.net>
2016-10-25 10:28:47 -0400

    whitespace

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,14 +1,14 @@
 module.exports = {
     dev: {
         files: {
-          'dist/js/homepage.scripts.js': ['src/js/*.js'],
-          'dist/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
+            'dist/js/homepage.scripts.js': ['src/js/*.js'],
+            'dist/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
         }
     },
     dist: {
         files: {
-          'tmp/js/homepage.scripts.js': ['src/js/*.js'],
-          'src/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
+            'tmp/js/homepage.scripts.js': ['src/js/*.js'],
+            'src/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
         }
     }  
 }
