e215852026df852307d910baf185e89dbd59b79f
Mark Reilly <mark@alienresident.net>
2016-10-31 10:56:41 -0400

    Added magnification for css images

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -1,10 +1,18 @@
 module.exports = {
-    all: {
+    content: {
         files: [{
             expand: true,
             cwd: 'tmp/',
             src: ['images/*.{png,jpg,gif}'],
             dest: 'dist/'
+        }]
+    },
+    style: {
+        files: [{
+            expand: true,
+            cwd: 'src/css/',
+            src: ['images/*.{png,jpg,gif}'],
+            dest: 'dist/css/'
         }]
     }
 };
