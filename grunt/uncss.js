a3d8d23f014e7d33b606059900358293878e3c1e
Mark Reilly <mark@alienresident.net>
2017-11-09 23:44:42 -0500

    fixing

--- grunt/uncss.js
+++ grunt/uncss.js
@@ -1,15 +1,12 @@
 module.exports = {
-    uncss: {
+    options: {
+        ignore: ['.wf-roboto-n4-active body', '.wf-roboto-n4-active h1' , '.wf-roboto-n4-active h2', '.wf-roboto-n3-active h1', '.wf-roboto-n3-active h2', '.wf-roboto-n1-active h1', '.wf-roboto-n3-active h2']
+        },
+    all: {
         options: {
-            ignore: ['.wf-roboto-i4-active i', '.wf-roboto-i4-active em', '.wf-roboto-n7-active b', '.wf-roboto-n7-active strong', '.wf-roboto-n4-active body', '.wf-robotoslab-n4-active h1', '.wf-robotoslab-n4-active h2', '.wf-robotoslab-n4-active h3', '.wf-robotoslab-n4-active h4']
-            },
-        all: {
-            options: {
-                report: 'min'
-            },
-            files: {
-                'tmp/css/all.uncss.css': ['tmp/index-process.html']
-            }
-        }
-    }  
-}
+            report: 'min'
+        },
+        src: ['tmp/index.html'],
+        dest: 'tmp/css/all.uncss.css'
+    }
+}  
