6bf3d09ee227cf8cc1d79795024a21d09abd6c68
Mark Reilly <mark@alienresident.net>
2017-11-09 23:41:42 -0500

    added css images

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,13 +1,28 @@
 module.exports = {
-    // dist: {
-    //     src: 'src/index.html',
-    //     dest: 'tmp/index.html',
-    //     options: {
-    //         process: function (content, srcpath) {
-    //             return content.replace('href="css/all.css"','href="../src/css/all.css"');
-    //         }
-    //     }
-    // },
+    dist: {
+        files: [
+            {expand: true, cwd: 'src/', src: ['*.{html,htm,php}'], dest: 'tmp/', filter: 'isFile'}
+        // src: 'src/index.html',
+        // dest: 'tmp/index.html',
+        // options: {
+        //     process: function (content, srcpath) {
+        //         //return content.replace('href="css/all.css"','href="../src/css/all.css"');
+        //     }
+        // }
+        ]
+    },
+    dev: {
+        files: [
+            {expand: true, cwd: 'src/', src: ['*.{html,htm,php}'], dest: 'dist/', filter: 'isFile'}
+        // src: 'src/index.html',
+        // dest: 'tmp/index.html',
+        // options: {
+        //     process: function (content, srcpath) {
+        //         //return content.replace('href="css/all.css"','href="../src/css/all.css"');
+        //     }
+        // }
+        ]
+    },
     images: {
         files: [
             // includes files within path
@@ -17,8 +32,8 @@ module.exports = {
     imagesHTML: {
         files: [
             // includes files within path
-            {expand: true, cwd: 'src/', src: ['images/**'], dest: 'tmp/'},
-            {expand: true, cwd: 'src/', src: ['*.{html,htm,php}'], dest: 'tmp/', filter: 'isFile'}
+            {expand: true, cwd: 'src/', src: ['images/**'], dest: 'tmp/'}//,
+            //{expand: true, cwd: 'src/', src: ['*.{html,htm,php}'], dest: 'tmp/', filter: 'isFile'}
       ]
     },
     imagesRes: {
