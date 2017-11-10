5ea24b1fe3e77cff8873d120fab27b1aa0e22a66
Mark Reilly <mark@alienresident.net>
2017-11-09 23:40:35 -0500

    removed js

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,14 +1,12 @@
 module.exports = {
     dev: {
         files: {
-            'dist/js/homepage.scripts.js': ['src/js/*.js'],
-            'dist/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
+             'dist/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
         }
     },
     dist: {
         files: {
-            'tmp/js/homepage.scripts.js': ['src/js/*.js'],
-            'src/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
+             'tmp/css/all.css': ['src/lib/normalize.css/normalize.css', 'src/css/global.css'],
         }
     }  
 }
