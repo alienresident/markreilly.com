c38730d610c1a4ace6b233294cf9a1401b2c4382
Mark Reilly <mark@alienresident.net>
2018-01-03 21:47:36 -0500

    Added srcset attributes
    generic values that will need to be revised later

--- grunt/responsive_images_extender.js
+++ grunt/responsive_images_extender.js
@@ -2,7 +2,20 @@ module.exports = {
   target: {
     options: {
       //baseDir: '~/Sites/markreilly.com'
-      srcAttribute: 'smallest'
+      srcAttribute: 'smallest',
+      sizes: [{
+          selector: '.portrait',
+          sizeList: [{
+            cond: 'max-width: 30em',
+            size: '100vw'
+          },{
+            cond: 'max-width: 50em',
+            size: '50vw'
+          },{
+            cond: 'default',
+            size: 'calc(33vw - 100px)'
+          }]
+        }]
     },
     files: [{
       expand: true,
